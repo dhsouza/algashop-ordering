90551a6a021a69ed453227fc00576b8fcc45353e
Danilo <danilohernandesdesouza@gmail.com>
2025-09-28 14:49:06 -0300

    chore: implementing value objects

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/FullName.java
@@ -0,0 +1,28 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public record FullName(String firstName, String lastName) {
+
+    public FullName(String firstName, String lastName) {
+        Objects.requireNonNull(firstName);
+        Objects.requireNonNull(lastName);
+
+        if (firstName.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (lastName.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.firstName = firstName.trim();
+        this.lastName = lastName.trim();
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/LoyaltyPoints.java
@@ -0,0 +1,44 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+import java.util.Objects;
+
+public record LoyaltyPoints(Integer value) implements Comparable<LoyaltyPoints> {
+
+    public LoyaltyPoints() {
+        this(0);
+    }
+
+    public LoyaltyPoints(Integer value) {
+        Objects.requireNonNull(value);
+
+        if (value < 0) {
+            throw new IllegalArgumentException("value cannot be negative");
+        }
+
+        this.value = value;
+    }
+
+    public LoyaltyPoints add(Integer value) {
+        return add(new LoyaltyPoints(value));
+    }
+
+    public LoyaltyPoints add(LoyaltyPoints loyaltyPoints) {
+        Objects.requireNonNull(loyaltyPoints);
+
+        if (loyaltyPoints.value() < 0) {
+            throw new IllegalArgumentException("loyaltyPoints cannot be negative");
+        }
+
+        return new LoyaltyPoints(this.value() + loyaltyPoints.value());
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public int compareTo(LoyaltyPoints o) {
+        return this.value().compareTo(o.value());
+    }
+}
